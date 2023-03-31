6f7ff9a3fe308776accc6eb9f4b1c8ddd00d66da
Peter <schmidt.multimedia@gmail.com>
2023-03-31 13:03:58 +0200

    Tests | Logische Aussage

--- script_01.js
+++ script_01.js
@@ -4,9 +4,19 @@
 
 // Deklaration + Assignment
 const ageJohn = 30;
-const ageMark = 20;
+const ageMark = 30;
 
 // Deklaration
 let isJohnOlder, isJohnEqual;
 
+// Test | Logische Aussage
+isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark)
+
+// Ausgabe
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
+console.log("------------------");
 
