02867ad58e4b09a91931b81a3371743bbf16c13b
Peter <schmidt.multimedia@gmail.com>
2023-04-03 10:56:04 +0200

    SWITCH|CASE 1

--- script_01.js
+++ script_01.js
@@ -14,11 +14,11 @@ isJohnOlder = (ageJohn > ageMark);
 isJohnEqual = (ageJohn == ageMark);
 
 // Ausgabe
-console.log("ageJohn: " + ageJohn);
-console.log("ageMark: " + ageMark);
-console.log("isJohnOlder: " + isJohnOlder);
-console.log("isJohnEqual: " + isJohnEqual);
-console.log("------------------");
+// console.log("ageJohn: " + ageJohn);
+// console.log("ageMark: " + ageMark);
+// console.log("isJohnOlder: " + isJohnOlder);
+// console.log("isJohnEqual: " + isJohnEqual);
+// console.log("------------------");
 
 /************ IF  ************/
 // TINA --> There is no alternative!
@@ -53,17 +53,50 @@ console.log("------------------");
 /************ IF - ELSE IF  ************/
 // mit alternativen Fällen (älter, jünger, gleich alt)
 
-if(isJohnOlder)
-{
-    console.log("John ist älter.");
-} 
-//  1...n Alternativen
-else if(isJohnEqual)
-{
-    console.log("John ist gleich alt.");
+// if(isJohnOlder)
+// {
+//     console.log("John ist älter.");
+// } 
+// //  1...n Alternativen
+// else if(isJohnEqual)
+// {
+//     console.log("John ist gleich alt.");
+// }
+// // wenn alle vorherigen Tests fehlschlagen ...
+// else 
+// {
+//     console.log("John ist jünger.");
+// }
+
+/****** Fallunterscheidung / SWITCH|CASE 1 ******/
+
+const firstName = "Jane";
+let job;
+
+job = "driver";     // .. fährt TAXI! / UBER
+job = "diver";      // .. taucht im Rhein! 
+job = "artist";     // .. malt ein Bild!
+job = "pilot";      // .. macht etwas anderes! --> default
+job = "teacher";    // .. unterrichtet!
+// job = "instructor";  // .. unterrichtet!
+
+switch (job) {
+    case "driver":
+        console.log(firstName + " fährt TAXI!");
+        break;
+    case "diver":
+        console.log(firstName + " taucht im Rhein!");
+        break;
+    case "artist":
+        console.log(firstName + " malt ein Bild!");
+        break;
+    case "teacher":
+    case "instructor":
+        console.log(firstName + " unterrichtet!");
+        break;
+    default:
+        console.log(firstName +  " macht etwas anderes!");
+        break;
 }
-// wenn alle vorherigen Tests fehlschlagen ...
-else 
-{
-    console.log("John ist jünger.");
-}
+
+
